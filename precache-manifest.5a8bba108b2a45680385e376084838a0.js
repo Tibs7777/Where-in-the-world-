1176b012acc1a3e98c747393dbda0a438eee1896
Tibs7777 <tom.bartlett2@btinternet.com>
2020-10-21 20:01:10 +0100

    first commit

---
+++ precache-manifest.5a8bba108b2a45680385e376084838a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14222369496456f493266ba6e207d633",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab08072cd534500d24c",
+    "url": "/static/css/main.90263952.chunk.css"
+  },
+  {
+    "revision": "1749c7bab58c2a99a59b",
+    "url": "/static/js/2.37138c54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.37138c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab08072cd534500d24c",
+    "url": "/static/js/main.13d98c8e.chunk.js"
+  },
+  {
+    "revision": "f4e3d70bd5967adc144d",
+    "url": "/static/js/runtime-main.a5f11eaf.js"
+  },
+  {
+    "revision": "11b68b17bd81d431d3b45cb46762d1e8",
+    "url": "/static/media/arrow-left1.11b68b17.svg"
+  },
+  {
+    "revision": "13b32cd355c23d165060e3acfd5e228e",
+    "url": "/static/media/chevron-small-down.13b32cd3.svg"
+  },
+  {
+    "revision": "e8aa9fe3effe97c94c6d61c11ab98c64",
+    "url": "/static/media/moon-o.e8aa9fe3.svg"
+  },
+  {
+    "revision": "bf3113cdf3a30207d9a5399b7aea0f0b",
+    "url": "/static/media/search.bf3113cd.svg"
+  },
+  {
+    "revision": "f48a33cde3c428db6c98932e90538a45",
+    "url": "/static/media/sun.f48a33cd.svg"
+  }
+]);
